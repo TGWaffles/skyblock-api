8f07a8f3991f5952edd1e63ab7addedd7d6a07f0
Thomas Wilcox <thomas.wilcox@hotmail.co.uk>
2024-10-19 02:07:19 +0100

    remove mongodb admin metrics

--- src/metrics.ts
+++ src/metrics.ts
@@ -16,40 +16,6 @@ const apiKeyCounter = new Gauge({
 		apiKeyCounter.set(keyUsage.lastMinute)
 	}
 })
-const connectionsCounter = new Gauge({
-	name: 'mongodb_current_connections',
-	help: 'Number of active connections to the database.',
-	registers: [ register ],
-	async collect() {
-		let status = await fetchServerStatus()
-
-		connectionsCounter.set(status.connections.current)
-		networkBytesInCounter.set(status.network.bytesIn)
-		networkBytesOutCounter.set(status.network.bytesOut)
-		readCounter.set(status.opLatencies.reads.ops)
-		writeCounter.set(status.opLatencies.writes.ops)
-	}
-})
-const networkBytesInCounter = new Gauge({
-	name: 'mongodb_network_bytes_in',
-	help: 'Number of bytes received by the database.',
-	registers: [ register ],
-})
-const networkBytesOutCounter = new Gauge({
-	name: 'mongodb_network_bytes_out',
-	help: 'Number of bytes sent by the database.',
-	registers: [ register ],
-})
-const readCounter = new Gauge({
-	name: 'mongodb_read_ops',
-	help: 'Number of read operations by the database.',
-	registers: [ register ],
-})
-const writeCounter = new Gauge({
-	name: 'mongodb_write_ops',
-	help: 'Number of write operations by the database.',
-	registers: [ register ],
-})
 const dbSizeCounter = new Gauge({
 	name: 'mongodb_db_size',
 	help: 'Size of the database in bytes.',
